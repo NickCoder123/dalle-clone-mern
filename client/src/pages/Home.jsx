4c63adfbbd282c814bf12fa94a68bb25da0db994
NickCoder123 <nikunjmehra935@gmail.com>
2023-06-06 18:04:18 +0530

    Added render server URL

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
       setLoading(true);
 
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://dalle-wqjk.onrender.com/api/v1/post', {
           headers: {
             'Content-Type': 'application/json'
           },
